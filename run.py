dbafb7654c70f8ffca7492df4f7112249ff2e661
Carvu94 <cmatej08@gmail.com>
2022-11-30 20:25:57 +0000

    Add a function to print the hangman depending on number of wrong guesses

--- run.py
+++ run.py
@@ -1,3 +1,55 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random 
+from words import words
+
+### Choose a random word
+randomWord = random.choice(words)
+
+for x in randomWord:
+    print('_', end=' ')
+
+def print_hangman(wrong):
+    """
+    Prints hangman depending on number of wrong guesses
+    """
+    if(wrong == 0):
+        print('\n|----+')
+        print('    |')
+        print('    |')
+        print('    |')
+        print('   ===')
+    elif(wrong == 1):
+        print('\n|----+')
+        print('O   |')
+        print('    |')
+        print('    |')
+        print('   ===')
+    elif(wrong == 2):
+        print('\n|----+')
+        print('O   |')
+        print('|   |')
+        print('    |')
+        print('   ===')
+    elif(wrong == 3):
+        print('\n|----+')
+        print(' O  |')
+        print('/|  |')
+        print('    |')
+        print('   ===')
+    elif(wrong == 4):
+        print('\n|----+')
+        print(' O  |')
+        print('/|\ |')
+        print('    |')
+        print('   ===')
+    elif(wrong == 5):
+        print('\n|----+')
+        print(' O  |')
+        print('/|\ |')
+        print('/   |')
+        print('   ===')
+    elif(wrong == 6):
+        print('\n|----+')
+        print(' O  |')
+        print('/|\ |')
+        print('/ \ |')
+        print('   ===')
