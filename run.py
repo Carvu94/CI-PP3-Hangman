bc96860d06f1a3abc19d48b10dca499b22bb0d9d
Carvu94 <cmatej08@gmail.com>
2022-11-30 22:48:09 +0000

    Add the play function that runs the game

--- run.py
+++ run.py
@@ -6,57 +6,126 @@ def get_word():
     """ 
     Get a random word and return
     """
-    randomWord = random.choice(words)
-    return randomWord.upper()
+    random_word = random.choice(words)
+    return random_word.upper()
 
+
+def play(random_word):
+    """
+    Function that runs the game 
+    Prints welcome message, hangman and word to guess
+    Checks if guess is correct and returns appropriate message
+    """
+    word_to_guess = '_ ' * len(random_word)
+    guessed = False
+    guessed_letters = []
+    guessed_words = []
+    lives = 6
+    print("Welcome to The Hangman! \nLet's play!")
+    print(print_hangman(lives))
+    print(word_to_guess)
+    print('\n')
+    while not guessed and lives > 0:
+        guess = input('Please guess a letter or a word: ').upper()
+        if len(guess) == 1 and guess.isalpha():
+            if guess in guessed_letters:
+                print(f'You already guessed letter {guess}')
+            elif guess not in random_word:
+                print(f'Letter {guess} is not in the word. Try again.')
+                lives -= 1
+                guessed_letters.append(guess)
+            else:
+                print(f'Amazing! {guess} is in the word!')
+                guessed_letters.append(guess)
+                word_list = list(word_to_guess)
+                indices = [i for i, letter in enumerate(random_word) if letter == guess]
+                for index in indices:
+                    word_list[index] = guess
+                word_to_guess = ''.join(word_list)
+                if "_" not in word_to_guess:
+                    guessed = True
+        elif len(guess) == len(random_word) and guess.isalpha():
+            if guess in guessed_words:
+                print(f'You already guessed the word {guess}')
+            elif guess != random_word:
+                print(f'{guess} is not the word.')
+                lives -= 1
+                guessed_words.append(guess)
+            else:
+                guessed = True
+                word_to_guess = random_word
+        else:
+            print('Not a valid guess')
+        print(print_hangman(lives))
+        print(word_to_guess)
+        print('\n')
+    if guessed:
+        print('Congratulations! You guessed the word! You win!')
+    else:
+        print(f'Sorry, you ran out of tries. The word was {random_word}. Better luck next time!')
 
 # for x in randomWord:
 #     print('_', end=' ')
 
 
-def print_hangman(wrong):
+def print_hangman(lives):
     """
     Prints hangman depending on number of wrong guesses
     """
-    if(wrong == 0):
+    if(lives == 6):
         print('\n|----+')
-        print('    |')
-        print('    |')
-        print('    |')
-        print('   ===')
-    elif(wrong == 1):
+        print('     |')
+        print('     |')
+        print('     |')
+        print('    ===')
+    elif(lives == 5):
         print('\n|----+')
-        print('O   |')
-        print('    |')
-        print('    |')
-        print('   ===')
-    elif(wrong == 2):
+        print('O    |')
+        print('     |')
+        print('     |')
+        print('    ===')
+    elif(lives == 4):
         print('\n|----+')
-        print('O   |')
-        print('|   |')
-        print('    |')
-        print('   ===')
-    elif(wrong == 3):
-        print('\n|----+')
-        print(' O  |')
-        print('/|  |')
-        print('    |')
-        print('   ===')
-    elif(wrong == 4):
-        print('\n|----+')
-        print(' O  |')
-        print('/|\ |')
-        print('    |')
-        print('   ===')
-    elif(wrong == 5):
-        print('\n|----+')
-        print(' O  |')
-        print('/|\ |')
-        print('/   |')
-        print('   ===')
-    elif(wrong == 6):
-        print('\n|----+')
-        print(' O  |')
-        print('/|\ |')
-        print('/ \ |')
-        print('   ===')
+        print('O    |')
+        print('|    |')
+        print('     |')
+        print('    ===')
+    elif(lives == 3):
+        print('\n |----+')
+        print(' O    |')
+        print('/|    |')
+        print('      |')
+        print('     ===')
+    elif(lives == 2):
+        print('\n |----+')
+        print(' O    |')
+        print('/|\   |')
+        print('      |')
+        print('     ===')
+    elif(lives == 1):
+        print('\n |----+')
+        print(' O    |')
+        print('/|\   |')
+        print('/     |')
+        print('     ===')
+    elif(lives == 0):
+        print('\n |----+')
+        print(' O    |')
+        print('/|\   |')
+        print('/ \   |')
+        print('     ===')
+
+
+def main():
+    """
+    Function that runs all functions and offer user to choose to play again. 
+    """
+    random_word = get_word()
+    play(random_word)
+    while input('Play again? (Y/N) ').upper() == 'Y':
+        random_word = get_word()
+        play(random_word)
+
+
+if __name__ == "__main__":
+    main()
